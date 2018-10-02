da86f6f404a3495b045653fdb4d59471c4c12bf5
Santosh <bsantosh909@gmail.com>
2018-10-02 09:47:42 +0545

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client } = require("discord.js");
+const client = new Client();
 var auth = require('./auth.json');
  
 client.on("ready", () => {
@@ -10,33 +10,19 @@ client.on("ready", () => {
 client.on("message", (message) => {
     if (message.content.startsWith("ping")) {
         message.channel.send("you spent 2 hours and forgot to put in the right token");
-    } else
-
-    if (message.content.startsWith("Hey bot, do we like helo?")) {
+    } else if (message.content.startsWith("Hey bot, do we like helo?")) {
         message.channel.send("Yes, we like helo");
-    } else
-
-    if (message.content.startsWith("Hey bot, do we like nervous?")){
+    } else if (message.content.startsWith("Hey bot, do we like nervous?")){
         message.channel.send("Yes, we like nervous")
-    }
-
-    if (message.content.startsWith("Hey bot, do we like people?")){
+    } else if (message.content.startsWith("Hey bot, do we like people?")){
         message.channel.send("Yes, we like everyone")
-    }
-
-    if (message.content.startsWith("Hey bot, do we like craig?")){
+    } else if (message.content.startsWith("Hey bot, do we like craig?")){
         message.channel.send("No, have you seen my source code?")
-    }
-
-    if (message.content.startsWith("Hey bot, respond to me")){
+    } else if (message.content.startsWith("Hey bot, respond to me")){
         message.channel.send("No")
-    }
-
-    if (message.content.startsWith("Hey bot, c++ is lame")){
+    } else if (message.content.startsWith("Hey bot, c++ is lame")){
         message.channel.send("You're not wrong")
-    }
-
-    if (message.content.startsWith("Hey bot, use a better language")){
+    } else if (message.content.startsWith("Hey bot, use a better language")){
         message.channel.send("What would you suggest? You know what I don't care nervous")
     }
 
